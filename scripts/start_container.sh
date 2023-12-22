05656c8281a4e72b99003a600867cc1da9f49374
chaitanya-online <chaithu.nampalli@gmail.com>
2023-12-22 10:17:38 +0530

    changes

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,5 @@ set -e
 docker pull richeb/python-aws-code:latest
 
 # Run the Docker image as a container
-docker run -p 8000:8000 richeb/python-aws-code python -m http.server
+docker run -p 8000:8000 --name python-server-aws richeb/python-aws-code python -m http.server
+
